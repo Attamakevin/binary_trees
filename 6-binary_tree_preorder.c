0e918d882f78dae0cd6def8f8caeff5aea642543
attama <kevinejikez@gmail.com>
2023-04-01 05:39:03 -0700

    task6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_preorder - function to implement preoder tranversal
+ * @tree: pointer to the root node
+ * @func: pointer to function to call for each node
+ * Return: return void
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
