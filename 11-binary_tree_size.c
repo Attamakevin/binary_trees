b35996854488be33ee33e60d267616b34d8a8a46
attama <kevinejikez@gmail.com>
2023-04-02 06:55:49 -0700

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_size - a function to find the size of binary tree
  * @tree: a pointer to the root node
- * Reurn: return the size of the tree
+ * Return: return the size of the tree
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
