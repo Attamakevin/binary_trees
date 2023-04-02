b5f3480f9979e0e7b4738a7eddccac96b4bb9c2a
attama <kevinejikez@gmail.com>
2023-04-02 05:15:16 -0700

    taskk 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,7 +9,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
 
-	if (tree == NULL || tree->parent == NULL /*|| (tree->left == NULL && tree->right == NULL)*/)
+	if (tree == NULL || tree->parent == NULL)
 	{
 		return (0);
 	}
